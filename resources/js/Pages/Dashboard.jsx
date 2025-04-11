1d9df58d7fb3283041866dfecd7e7c91ef5d3c8f
ulises <umendoza2078@gmail.com>
2025-04-11 10:51:18 -0400

    feat: changed dashboard mesagge

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -14,7 +14,7 @@ export default function Dashboard({ auth }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">¡Has iniciado sesión!</div>
+                        <div className="p-6 text-gray-900">¡Bienvenido al sistema de Gestión Académica!</div>
                         {/* Aquí puedes añadir contenido específico de tu dashboard más adelante */}
                     </div>
                 </div>
